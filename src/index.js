3d92e086057723af448fea890ff376d7fb96e874
JakubKarp <jakub.karp@innespacery.pl>
2018-07-17 14:33:32 +0200

    clean commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyApupIEL7Wc1ShQ-uqhGa0dA_rsbkvN2I4';
+const API_KEY = 'xx';
 
 // Dzięki temu poniżej możemy w konsoli zobaczyć objekty z YT ze wszystkimi kluczami
 /* YTSearch({key: API_KEY, term: 'polar'}, function(data) {
@@ -54,6 +54,56 @@ class App extends Component {
     }
 }
 
+
+/* 
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('polar');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, videos => {
+            console.log(videos)
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });        
+    }
+
+
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
+            </div>
+        );
+    }
+}
+
+ */
+
+
+
+
+
+//put html on page (in DOM)
+ReactDOM.render(<App />, document.querySelector('.container')); 
+
+
 // produce some html
 //To jest wersja komponentu funkcyjna. Powyżej jest klasowa wersja komponentu
 /* const App = () => {
@@ -63,7 +113,3 @@ class App extends Component {
         </div>
     );
 } */
-
-
-//put html on page (in DOM)
-ReactDOM.render(<App />, document.querySelector('.container')); 
